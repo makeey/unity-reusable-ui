c59bf9fd396a1178b104f7e19155a4b232af798a
Anton Makieiev <makeey97@gmail.com>
2020-01-30 21:37:39 +0100

    Initial commit

---
+++ Assets/Scripts/UIScreen.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Makeey.UI
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(CanvasGroup))]
+    public class UIScreen : MonoBehaviour
+    {
+        [SerializeField]
+        private Selectable selectable;
+        [SerializeField]
+        private UnityEvent onShowScreenEvent;
+        [SerializeField]
+        private UnityEvent onCloseScreenEvent;
+        private Animator animator;
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            if (selectable)
+            {
+                EventSystem.current.SetSelectedGameObject(selectable.gameObject);
+            }
+        }
+
+        public void ShowScreen()
+        {
+            onShowScreenEvent?.Invoke();
+            HandleAnimation("show");
+        }
+
+        public void CloseScreen()
+        {
+            onCloseScreenEvent?.Invoke();
+            HandleAnimation("close");
+        }
+
+        private void HandleAnimation(string trigger)
+        {
+            if (animator)
+            {
+                animator.SetTrigger(trigger);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UISystem.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Makeey.UI
+{
+    public class UISystem : MonoBehaviour
+    {
+        #region Parameters that are set from Unity
+        [SerializeField] private UIScreen StartScreen;
+        [SerializeField] private UnityEvent onSwitchedScreenEvent;
+        [SerializeField] private Image faderImage;
+
+        [Range(0, 5f)]
+        [SerializeField] private float faderInDuration = 1f;
+        [Range(0, 5f)]
+        [SerializeField] private float faderOutDuration = 1f;
+        #endregion
+
+        private UIScreen[] screens = new UIScreen[0];
+        private UIScreen currentScreen;
+        public UIScreen CurrentScreen => currentScreen;
+        private UIScreen previousScreen;
+        private void Start()
+        {
+            screens = GetComponentsInChildren<UIScreen>(false);
+            SetAllScreenActive();
+            if (StartScreen)
+            {
+                SwitchScreen(StartScreen);
+            }
+            if (faderImage)
+            {
+                faderImage.gameObject.SetActive(true);
+            }
+
+            FadeIn();
+        }
+
+        public void SwitchScreen(UIScreen screen)
+        {
+            if (!screen)
+            {
+                return;
+            }
+
+            if (currentScreen)
+            {
+                currentScreen.CloseScreen();
+                previousScreen = currentScreen;
+            }
+
+            currentScreen = screen;
+            currentScreen.ShowScreen();
+            onSwitchedScreenEvent?.Invoke();
+        }
+
+        private void SetAllScreenActive()
+        {
+            foreach (var screen in screens)
+            {
+                screen.gameObject.SetActive(true);
+            }
+        }
+
+        private void FadeIn()
+        {
+            if (!faderImage)
+            {
+                return;
+            }
+
+            faderImage.CrossFadeAlpha(0f, faderInDuration, false);
+        }
+
+        private void FadeOut()
+        {
+            if (!faderImage)
+            {
+                return;
+            }
+
+            faderImage.CrossFadeAlpha(1f, faderOutDuration, false);
+        }
+    }
+}
